3f0896620c25b2ea79daaed37b6f2e19ef120cbe
Juan Guajardo <gigfmedia@icloud.com>
2025-02-11 16:42:39 -0300

    Update express.js

--- api/express.js
+++ api/express.js
@@ -1,4 +1,4 @@
-const express = require("express");
+/*const express = require("express");
 const fetch = require("node-fetch");
 const path = require("path");
 
@@ -20,4 +20,41 @@ app.post('/data', (req, res) => {
     });
 });
 
+module.exports = app;*/
+
+
+const express = require('express');
+const fetch = require('node-fetch');
+const path = require('path');
+
+const app = express();
+
+// Middleware para parsear JSON
+app.use(express.json());
+
+// Endpoint proxy
+app.post('/api/proxy/search', async (req, res) => {
+    try {
+        const apiResponse = await fetch('https://apim.workato.com/api_ferco/searchi-v1/search', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'api-token': '94700300d77ce83c0806ec2e9cec6d16f24522b9a49bf4e9638d7efcc1e39710'
+            },
+            body: JSON.stringify(req.body),
+        });
+
+        const data = await apiResponse.text(); // o .json() si la respuesta es JSON
+        res.send(data);
+    } catch (error) {
+        console.error('Error en el proxy:', error);
+        res.status(500).send('Error al obtener la API');
+    }
+});
+
+app.get("/", (req, res) => {
+  res.json({ message: "¡Hola desde Express en Vercel! 🚀" });
+});
+
+// Exportar el handler para Vercel
 module.exports = app;
